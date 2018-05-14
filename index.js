895a8a4bf3e58d3132b5a8e3aa3c9f57fdee4e92
jwhite88 <jdwhite0502@gmail.com>
2018-05-14 19:00:08 -0400

    Modify title text

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 // Get first element with the element 'button'
 //const button = document.querySelector('button');
 
-// Get element with the id 'megaButton' (#)
-//const button = document.querySelector('#megaButton');
+// Get element with the id 'changeTextButton' (#)
+//const button = document.querySelector('#changeTextButton');
 
 // Get first element with the class 'greeting' (.)
 //const button = document.querySelector('.greeting');
 
 // Get first element with the element 'button' and class 'greeting'
 const button = document.querySelector('button.greeting');
+
 function changeText(ev) {
-    /*// Get first element with the element 'p' and class 'greeting'
+    
     const paragraph = document.querySelector('p.greeting');
-
-    // Set <p> tag's text
-    paragraph.textContent = "You done clicked!";*/
-
-    // Text inside button/event that clicked it
-    ev.target.textContent = "Clickity Click";
-
-
-
-
+    const title = document.querySelector('h1#title');
+    // Change text of first <p> element with class 'greeting'
+    paragraph.textContent = "Text has changed!";
+    // Change text of first <h1> element with class 'title'
+    title.textContent = "New User Directory";
+    // Change text of the element that called the function
+    ev.target.textContent = "Change Text Again";
 }
 
-// Run changeText button when button clicked
+// Run changeText function when button clicked
 button.addEventListener('click', changeText);
