203614859da76a6c3db1103809209dc376a49b9a
jdwhite88 <white570@purdue.edu>
2018-05-15 16:04:45 -0400

    Display users in an unordered list

--- index.js
+++ index.js
@@ -9,14 +9,28 @@ const handleSubmit = function(ev) {
     const users = document.querySelector('#users');
     const form = ev.target;
 
-    const username = form.userName.value;
+    const userName = form.userName.value;
     const age = form.age.value;
     const color = form.favoriteColor.value;
 
-    const p = document.createElement('p');
-    p.textContent = `${username}, ${age}`;
-    p.style.backgroundColor = color;
-    users.appendChild(p);
+    // Adding to div in HMTL:
+    //  <ul>
+    //     <li>Name: [username]</li>
+    //     <li>Age: [age]</li>
+    //     <li></li>
+    // </ul>
+    const list = document.createElement('ul');
+    const nameItem = document.createElement('li');
+    nameItem.textContent = `Name: ${userName}`;
+    const ageItem = document.createElement('li');
+    ageItem.textContent = `Age: ${age}`;
+
+    nameItem.style.backgroundColor = color;
+    ageItem.style.backgroundColor = color;
+    
+    list.appendChild(nameItem);
+    list.appendChild(ageItem);
+    users.appendChild(list);
 
     form.reset();
     form.userName.focus();
