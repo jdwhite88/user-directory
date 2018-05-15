4c6e17bb6510ebd7344808b802b7d9129097d561
jdwhite88 <white570@purdue.edu>
2018-05-15 15:25:13 -0400

    Add age to name and change focus

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // First <button> element with class 'greeting'
 const button = document.querySelector('button.greeting');
+// First element with id 'userForm'
 const form = document.querySelector('#userForm');
 
 const handleSubmit = function(ev) {
-    // Prevent submit button from refreshing pahe
+    // Prevent submit button from refreshing page
     ev.preventDefault();
     const users = document.querySelector('#users');
-    const userName = ev.target.user.value;
-    users.innerHTML += '<p>' + userName + '</p>';
-    ev.target.user.value = '';
+    const form = ev.target;
+
+    const userName = form.user.value;
+    const age = form.age.value;
+    users.innerHTML += `<p>${userName}, ${age}<\p>`;
+
+    form.reset();
+    form.userName.focus();
 }
 form.addEventListener('submit', handleSubmit);
