451ac75415023ec6cde9baf2e9df53cd9080e677
jdwhite88 <white570@purdue.edu>
2018-05-15 14:52:35 -0400

    Add each user in its own paragraph

--- index.js
+++ index.js
@@ -7,30 +7,7 @@ const handleSubmit = function(ev) {
     ev.preventDefault();
     const users = document.querySelector('#users');
     const userName = ev.target.user.value;
-    users.textContent += userName + ' ';
+    users.innerHTML += '<p>' + userName + '</p>';
     ev.target.user.value = '';
 }
 form.addEventListener('submit', handleSubmit);
-
-
-function changeText(ev) {
-    
-    // First <p> element with class 'greeting'
-    const paragraph = document.querySelector('p.greeting');
-    // First <h1> element with class 'heading'
-    const titleHeading = document.querySelector('h1.heading');
-    // Element with id 'message'
-    const messageHeading = document.querySelectorAll('.heading')[1];
-    // Text input form for updating heading
-    const newDirField = document.querySelector("#user");
-
-    paragraph.textContent = "Directory has changed!";
-    titleHeading.textContent = newDirField.value;
-    newDirField.value = '';
-    messageHeading.textContent = "Changed Message";
-
-    ev.target.textContent = "Change Text Again";
-}
-
-// Run changeText function when button clicked
-button.addEventListener('click', changeText);
