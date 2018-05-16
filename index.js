05039e881ef229650748c66202fcb2e87e3254bc
jdwhite88 <white570@purdue.edu>
2018-05-16 11:09:48 -0400

    Select color by clicking other users' swatches

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const handleSubmit = function(ev) {
     const users = document.querySelector('#users');
     const form = ev.target;
 
-    const list = renderList(form);
+    const list = renderList();
     users.appendChild(list);
 
     form.reset();
@@ -19,9 +19,11 @@ const handleSubmit = function(ev) {
 // Takes the color value from the color selector, coloring and returning a div.
 function renderColor(color) {
     const colorSwatch = document.createElement('div');
+    colorSwatch.className = 'swatch';
     colorSwatch.style.width = '6rem';
     colorSwatch.style.height = '3rem';
     colorSwatch.style.backgroundColor = color;
+    colorSwatch.addEventListener('click', updateColor);
     return colorSwatch;
 }
 
@@ -39,7 +41,7 @@ function renderListItem(name, input) {
 //     <li>Age: [age]</li>
 //     <li>Favorite Color: #xxxxxx <div style="background-color:#xxxxxx; height: 3rem; width: 6rem;"></div></li>
 // </ul>
-function renderList(form) {
+function renderList() {
     const userName = form.userName.value;
     const age = form.age.value;
     const color = form.favoriteColor.value;
@@ -56,6 +58,37 @@ function renderList(form) {
     list.appendChild(colorItem);
 
     return list;
+}
+
+//Change the color of the input to the swatch the user clicked
+function updateColor(ev) {
+    form.favoriteColor.value = parseRGB(ev.target.style.backgroundColor);
+}
+
+//Convert "rgb(r, g, b)" (base-10 string) to "#rrggbb" (base-16 string)
+function parseRGB(rgb) {
+    //Match 'rgb(#, #, #)' for the component numbers
+    const rgbMatch = rgb.match(/rgb\((\d+),\s*(\d+),\s*(\d+)\)/);
+    const red = rgbMatch[1];
+    const green = rgbMatch[2];
+    const blue = rgbMatch[3];
+    return rgbToHex(red, green, blue);
+}
+
+//Convert base-10 RGB colors (numbers) to base-16 color code (string)
+function rgbToHex(r, g, b) {
+    let hex = "#";
+    hex += componentToHex(r) + componentToHex(g) + componentToHex(b);
+    return hex;
+}
+
+//Convert base-10 color component (number) to base-16 (string)
+function componentToHex(rgbComp) {
+    let hexComp = Number(rgbComp).toString(16);
+    if (hexComp.length < 2) {
+        hexComp = '0' + hexComp;
+    }
+    return hexComp;
 }
 
 form.addEventListener('submit', handleSubmit);
